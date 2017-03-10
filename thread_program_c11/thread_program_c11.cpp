309edd31c230ff7c0b315bc72da4726c13e0d00c
TColorCat <2801714044@qq.com>
2017-03-10 15:08:53 +0800

    accumulate并行计算的demo

--- thread_program_c11/thread_program_c11.cpp
+++ thread_program_c11/thread_program_c11.cpp
@@ -6,32 +6,57 @@
 #include<iostream>
 using namespace std;
 
+
+void fun(int since)
+{
+	for (int i = 0; i < since; i++)cout << "since" << endl;
+
+}
 void fun()
 {
 	for (int i = 0; i < 10; i++)cout << "myfirst" << endl;
 
 }
+class X
+{
+public:
+	void since(){ cout << "sin" << endl; }
+};
 int _tmain(int argc, _TCHAR* argv[])
 {
+	/*void(*it)(void) = fun;
 	//用函数对象启动线程
 	//直接用lambda函数
 		thread my_first([]{ for (int i = 0; i < 10; i++)cout << "myfirst" << endl; });
 	//使用函数自动生成的对象
-		thread myfirst((fun));
-		thread myfirst{ fun };
-	//用function也一样
-		function<void(void)> thread_function = fun;
+		thread myfirst((it));
+		thread myfirst{ it };
+	//用function也一样 其中注意重载函数是不能直接导入的function的。要么用函数指针，要么用lambda，一般来说使用函数指针或者用static_cast
+		
+		function<void(void)> thread_function = it;
 		thread myfirst(thread_function);
 
+		
+	//function<void(int)> thread_function (fun);
+	//thread myfirst((fun),5);
 
 	//向线程函数传递参数
-	 for (int i = 0; i < 10; i++)cout << "mymain" << endl;
+
+
+
+	X my;
+	thread myfirst(&X::since, &my);
+	
+	for (int i = 0; i < 10; i++)cout << "mymain" << endl;
 
 
 	
 
 
 	myfirst.join();
+	*/
+
+	cout << thread::hardware_concurrency() << endl;
 	return 0;
 }
 
